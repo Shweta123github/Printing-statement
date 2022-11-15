212dcf04251a21b3058a1262fa850b160197e9e7
shweta <shwatakarvanjikar@gmail.com>
2022-11-15 13:35:25 +0530

    my first commit

---
+++ project/src/com/project/shweta/print.java
@@ -0,0 +1,10 @@
+package com.project.shweta;
+
+public class print {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("my first project");
+	}
+
+}
