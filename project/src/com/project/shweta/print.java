5aa9da378e100dafedd9e4b59b658c3b774c2556
shweta <shwatakarvanjikar@gmail.com>
2022-11-15 14:01:14 +0530

    my second commit

--- project/src/com/project/shweta/print.java
+++ project/src/com/project/shweta/print.java
@@ -5,6 +5,7 @@ public class print {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("my first project");
+System.out.println("second commit");
 	}
 
 }
